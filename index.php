c374ca4059438a55be1d51ee36170337f7c89725
Trần Đức Linh <41521233+lantrinh1999@users.noreply.github.com>
2020-03-25 13:35:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
+require './LHelper.php';
+$helper = new LHelper;
+
 $filenames = glob('*.jpg');
-var_dump($filenames);
+if(count($filenames) > 10) {
+  for ($i = 0; $i < count($filenames) - 1; $i++) {
+	  unlink($filenames[$i]);
+  }
+}
+$link = trim(strip_tags($_GET['u']));
+if (empty($link)) {
+  return false;
+}
+$im = imagecreatefromwebp($link);
+$file_name = basename($link);
+$a = imagejpeg($im, 'trim(trim($file_name, '.webp')).'.jpg', 80);
+echo !empty($a) ? true : false;
+die();
+
+
+
+
 ?>
